22fe17a30ddeeb19210b7ba07b4ed29656a5fc9e
Natanael Vitorino <64169072+natanael19vitorino@users.noreply.github.com>
2021-09-15 22:26:29 -0300

    Description Created

--- Time/src/Main.java
+++ Time/src/Main.java
@@ -1,3 +1,17 @@
+/* 
+    Faça um programa que receba um único valor representando segundos. 
+    Converter os segundos para horas, minutos e segundos.Utilizar os 
+    conceitos de OO para desenvolver uma solução.
+
+    Classe??
+    Atributos??
+    Métodos??
+    
+    Exemplo:
+    Entrada em segundos: 290
+    Saída: 0h4m50s
+*/
+
 import java.util.*;
 
 public class Main {
