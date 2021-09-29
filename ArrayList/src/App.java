c11c3a556cafa797678f5120c429e8beb702e4bb
Natanael Vitorino <64169072+natanael19vitorino@users.noreply.github.com>
2021-09-29 20:27:11 -0300

    References added

--- ArrayList/src/App.java
+++ ArrayList/src/App.java
@@ -7,26 +7,28 @@ public class App {
         ArrayList <Pessoa> l = new ArrayList <Pessoa>();
         // referência
         Pessoa rp;
-
         rp = p;
 
         System.out.println("Nome de p: "+ p.getNome());
         System.out.println("Nome de rp: "+ rp.getNome());
 
         rp = p1;
+        l.add(p);
         System.out.println("Nome de rp: "+ rp.getNome());
 
         System.out.println("Tamanho da lista: " + l.size());
 
         // inserção do elemento pessoa
-        l.add(p);
+        l.add(p1);
         System.out.println("Tamanho da lista: " + l.size());
 
         l.add(new Pessoa("Rita", 21));
         System.out.println("Tamanho da lista: " + l.size());
 
         // Leitura dos dados do ArrayList
-        
+        System.out.println("Pocisão 1: " + l.get(0).getNome());
+        System.out.println("Pocisão 2: " + l.get(1).getNome());
+        System.out.println("Pocisão 2: " + l.get(2).getNome());
         
     }
 }
