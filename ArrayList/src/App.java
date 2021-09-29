06b7d76cda319f6a644f17960260e701636b41ee
Natanael Vitorino <64169072+natanael19vitorino@users.noreply.github.com>
2021-09-29 20:41:56 -0300

    Middle inserction done

--- ArrayList/src/App.java
+++ ArrayList/src/App.java
@@ -26,9 +26,27 @@ public class App {
         System.out.println("Tamanho da lista: " + l.size());
 
         // Leitura dos dados do ArrayList
-        System.out.println("Pocisão 1: " + l.get(0).getNome());
-        System.out.println("Pocisão 2: " + l.get(1).getNome());
+        System.out.println("Pocisão 0: " + l.get(0).getNome());
+        System.out.println("Pocisão 1: " + l.get(1).getNome());
         System.out.println("Pocisão 2: " + l.get(2).getNome());
-        
+
+        // Inserir no meio da lista
+        l.add(1, new Pessoa("Jose", 25));
+        // Leitura dos dados do ArrayList
+        System.out.println("Pocisão 0: " + l.get(0).getNome());
+        System.out.println("Pocisão 1: " + l.get(1).getNome());
+        System.out.println("Pocisão 2: " + l.get(2).getNome());
+        System.out.println("Pocisão 3: " + l.get(3).getNome());
+
+
+        // remover Natanael
+        l.remove(p); // referencia
+        System.out.println("Tamanho da lista: " + l.size());
+        l.remove(0); // index
+        System.out.println("Tamanho da lista: " + l.size());
+        // Leitura dos dados do ArrayList
+        System.out.println("Pocisão 1: " + l.get(0).getNome());
+        l.remove(0); // index
+        System.out.println("ArrayList vazio? \n" + l.isEmpty());
     }
 }
